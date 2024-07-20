43f25f4c3ed9d02492cc1fb16886b6a5ee990ffc
nakashima-takeo <takeo555555555555555@yahoo.co.jp>
2024-07-20 22:58:08 +0900

    map

--- src/grammer/map.ts
+++ src/grammer/map.ts
@@ -1,3 +1,4 @@
 const array = [10, 20, 40];
 
-array.map((number, index) => console.log(`${index+1}番目は${number}です。`))
+export const arrayFunction = (array: number[]) => array.map((number, index) => console.log(`${index+1}番目は${number}です。`))
+arrayFunction(array);
