52b703e057a333dbf4d5f69aa34efb8db86bc6a2
timer <takeo555555555555555@yahoo.co.jp>
2024-09-06 22:25:46 +0900

    css

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+    plugins: [react({
+      jsxRuntime: 'automatic'  // automatic JSXトランスフォームを有効化
+    })],
     root: 'src',
     
     build:{
